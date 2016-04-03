76cf03bdc46d0becf797a2b9f96a7668778b70ee
srzm <ldlong@mail.ustc.edu.cn>
2016-04-03 19:33:59 +0800

    wait for an ubuntu to compile

---
+++ 16HUAWEI_CodeCraft/ProblemSet/HUAWEI Code Craft 2016 初赛赛题包/SDK-gcc-1.0/build/CMakeFiles/future_net.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/future_net.dir/future_net.cpp.o"
+  "CMakeFiles/future_net.dir/io.cpp.o"
+  "CMakeFiles/future_net.dir/route.cpp.o"
+  "/home/lidinglong/github/AsZ/16HUAWEI_CodeCraft/ProblemSet/HUAWEI Code Craft 2016 初赛赛题包/SDK-gcc-1.0/bin/future_net.pdb"
+  "/home/lidinglong/github/AsZ/16HUAWEI_CodeCraft/ProblemSet/HUAWEI Code Craft 2016 初赛赛题包/SDK-gcc-1.0/bin/future_net"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/future_net.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
