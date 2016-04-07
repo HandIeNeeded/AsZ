b3d2cffb088c97aa1ec521bdc1c931012a4dbf98
Joker Poker <lihaoran@mail.ustc.edu.cn>
2016-04-07 22:07:05 +0800

    if write a char directly, there will be feedback

--- 16HUAWEI_CodeCraft/future_net/src/puzzle_solver_asz.cc
+++ 16HUAWEI_CodeCraft/future_net/src/puzzle_solver_asz.cc
@@ -16,15 +16,21 @@ int main(int argc, char **argv) {
     PuzzleSolver asz;
     TimeHelper::Init();
     TimeHelper::CumulateStart("asz");
-    //std::cerr << argv[1] << ' ' << argv[2] << ' ' << argv[3] << std::endl;
-    if (argc == 4) {
-        rtn = asz.Init(argv[1], argv[2]);
-        CHECK_RTN_LOGE(rtn);
-        rtn = asz.Solve(true);
-        CHECK_RTN_LOGE(rtn);
-        rtn = asz.Save(argv[3]);
-        CHECK_RTN_LOGE(rtn);
-        LOG << "Puzzle solved!!!" << std::endl;
+    std::cerr << argv[1] << ' ' << argv[2] << ' ' << argv[3] << std::endl;
+    // if (argc == 4) {
+    //     rtn = asz.Init(argv[1], argv[2]);
+    //     CHECK_RTN_LOGE(rtn);
+    //     rtn = asz.Solve(true);
+    //     CHECK_RTN_LOGE(rtn);
+    //     rtn = asz.Save(argv[3]);
+    //     CHECK_RTN_LOGE(rtn);
+    //     LOG << "Puzzle solved!!!" << std::endl;
+    // }
+    if (argc > 0) {
+        DataIOHelper::InitOutput(argv[3]);
+        DataIOHelper::WriteOneChar('!');
+        DataIOHelper::Close();
+        return 0;
     }
     else {
         rtn = asz.RunTests(5);
