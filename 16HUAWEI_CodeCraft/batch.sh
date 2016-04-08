5faa68809232eadb55ed8ad13b1837dee7435787
Joker Poker <lihaoran@mail.ustc.edu.cn>
2016-04-08 14:55:23 +0800

    remove redundant files, write a bash to pack

--- 16HUAWEI_CodeCraft/batch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#cp batch.sh future_net
-cd future_net
-rm ./bin/* -rf
-make
-cp ./bin/puzzle_solver_asz ./future_net
-cd -
-tar -zcPf future_net.tar.gz ./future_net
-
---
+++ 16HUAWEI_CodeCraft/future_net/pack.sh
@@ -0,0 +1,3 @@
+cp ./bin/puzzle_solver_asz ./future_net
+tar -zcPf future_net.tar.gz ./include ./src ./Makefile ./future_net
+rm future_net
